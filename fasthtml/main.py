5a3106d5869fa14c02792da1f57dcd4289ae03e4
Hamel Husain <hamel.husain@gmail.com>
2024-09-09 15:56:46 -0700

    save

--- fasthtml/main.py
+++ fasthtml/main.py
@@ -1,25 +1,30 @@
 from fasthtml.common import *
-from httpx import get
+import httpx
 
 app, rt = fast_app()
 
 @rt("/")
 def get():
-    return Titled("GitHub Email Finder", 
-        A("Code on GitHub", href="https://github.com/hamelsmu/gh-email/tree/main/fasthtml"),
-        Form(hx_post="/get_emails", hx_target="#results")(
-            Label("GitHub Username", Input(name="username")),
-            Button("Get Emails", type="submit")
-        ),
-        Div(id="results")
-    )
+    return (
+        Title("GitHub Email Finder"), 
+        Main(
+            Grid(H1("GitHub Email Finder"),
+                Div(A("See Code on GitHub", href="https://github.com/hamelsmu/gh-email/tree/main/fasthtml"), style="text-align: right;")
+            ),
+            Form(hx_post="/get_emails", hx_target="#results")(
+                Input(name="username", placeholder="Enter a GitHub username"),
+                Button("Get Emails", type="submit")
+            ),
+            Div(id="results"),
+       cls="container")
+)
 
 @rt("/get_emails")
 def post(username: str):
     if username.strip() == 'hamelsmu':
         return P("@hamelsmu is the author of this app, so he has the privilege of showing you this message instead :)")
     
-    events = get(f"https://api.github.com/users/{username}/events/public", timeout=10).json()    
+    events = httpx.get(f"https://api.github.com/users/{username}/events/public", timeout=10).json()   
     emails = set()
     for event in events:
         for commit in event.get('payload', {}).get('commits', []):
