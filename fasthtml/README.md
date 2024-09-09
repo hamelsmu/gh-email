4e2719ba21a8085678b7aff56b56ca7dd51b3f13
Hamel Husain <hamel.husain@gmail.com>
2024-09-09 15:45:29 -0700

    save

--- fasthtml/README.md
+++ fasthtml/README.md
@@ -1,6 +1,18 @@
-To run this app:
+To run this app locally:
 
 ```bash
 pip install -U python-fasthtml
 python main.py
+```
+
+To deploy this app:
+
+```bash
+fh_railway_deploy gh-email
+```
+
+To refresh the deployed app
+
+```
+railway up -c
 ```
--- fasthtml/main.py
+++ fasthtml/main.py
@@ -1,25 +1,25 @@
 from fasthtml.common import *
-import requests
+from httpx import get
 
 app, rt = fast_app()
 
 @rt("/")
 def get():
     return Titled("GitHub Email Finder", 
-        Form(method="post", action="/get_emails")(
+        A("Code on GitHub", href="https://github.com/hamelsmu/gh-email/tree/main/fasthtml"),
+        Form(hx_post="/get_emails", hx_target="#results")(
             Label("GitHub Username", Input(name="username")),
             Button("Get Emails", type="submit")
-        )
+        ),
+        Div(id="results")
     )
 
 @rt("/get_emails")
 def post(username: str):
     if username.strip() == 'hamelsmu':
-        return Titled("Special Message", 
-            P("@hamelsmu is the author of this app, so he has the privilege of showing you this message instead :)")
-        )
+        return P("@hamelsmu is the author of this app, so he has the privilege of showing you this message instead :)")
     
-    events = requests.get(f"https://api.github.com/users/{username}/events/public", timeout=10).json()    
+    events = get(f"https://api.github.com/users/{username}/events/public", timeout=10).json()    
     emails = set()
     for event in events:
         for commit in event.get('payload', {}).get('commits', []):
@@ -29,10 +29,8 @@ def post(username: str):
                 emails.add(f"{name}: {email}")
     
     if emails:
-        return Titled("Email Results", 
-            Ul(*[Li(email) for email in emails])
-        )
+        return Ul(*[Li(email) for email in emails])
     else:
-        return Titled("No Results", P("No emails found"))
+        return P("No emails found")
 
 serve()
