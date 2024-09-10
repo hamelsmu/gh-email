a786a75996fd4b04e0e1fa27c254259a27017733
Hamel Husain <hamel.husain@gmail.com>
2024-09-10 15:05:40 -0700

    save

--- fasthtml/assets/copy.js
+++ fasthtml/assets/copy.js
@@ -1,16 +1,22 @@
 function copyToClipboard(event, email) {
-    halt(event);
-    navigator.clipboard.writeText(email)
-      .then(() => {
-        const btn = me('.copy-btn').classAdd('copied');
-        const textSpan = me('.button-text').textContent('Copied');                  
-        const icon = btn('i').classAdd('fas fa-check green-check');
-                  
-        sleep(1500, () => {
-          btn.classRemove('copied');
-          textSpan.textContent = 'Copy';
-          icon.classList = 'fas fa-copy';
-        });
-      })
-      .catch(err => {console.error('Failed to copy text: ', err)});
+  event.preventDefault();
+  navigator.clipboard.writeText(email)
+    .then(() => {
+      const btn = event.target.closest('.copy-btn');
+      btn.classList.add('copied');
+      const textSpan = btn.querySelector('.button-text');
+      textSpan.textContent = 'Copied';
+
+      const icon = btn.querySelector('i');
+      icon.classList = 'fas fa-check green-check';
+
+      setTimeout(() => {
+        btn.classList.remove('copied');
+        textSpan.textContent = 'Copy';
+        icon.classList = 'fas fa-copy';
+      }, 1500);
+    })
+    .catch(err => {
+      console.error('Failed to copy text: ', err);
+    });
 }
